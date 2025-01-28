ff399f03a87365ba4ad150b55ddc388793ee09ed
CPenalba <carolinapenalbacorpa@gmail.com>
2025-01-28 14:12:32 +0100

    Conjetura Collatz

--- PrimerMvcNetCore/Controllers/MatematicasController.cs
+++ PrimerMvcNetCore/Controllers/MatematicasController.cs
@@ -16,6 +16,34 @@ namespace PrimerMvcNetCore.Controllers
             return View();
         }
 
+        public IActionResult ConjeturaCollatz()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ConjeturaCollatz(int numero)
+        {
+            //DEBEMOS DEVOLVER UN OBJETO COMPLEJO CON 
+            //UNA LISTA DE NUMEROS
+            List<int> numeros = new List<int>();
+            while (numero != 1)
+            {
+                if (numero % 2 == 0)
+                {
+                    numero = numero / 2;
+                }
+                else
+                {
+                    numero = numero * 3 + 1;
+                }
+                numeros.Add(numero);
+            }
+            //DEVOLVEMOS EL MODEL A LA VISTA
+            return View(numeros);
+
+        }
+
         public IActionResult SumarNumerosPost()
         {
             return View();
