febba8631961dee7a0be1d20098d9399b78894a9
CPenalba <carolinapenalbacorpa@gmail.com>
2025-01-28 11:34:19 +0100

    Persona

--- PrimerMvcNetCore/Controllers/InformacionController.cs
+++ PrimerMvcNetCore/Controllers/InformacionController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PrimerMvcNetCore.Models;
+using System.Globalization;
 
 namespace PrimerMvcNetCore.Controllers
 {
@@ -7,6 +8,35 @@ namespace PrimerMvcNetCore.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+
+        public IActionResult VistaControllerPost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult VistaControllerPost(Persona p, string aficiones)
+        {
+            ViewData["DATA"] = "Nombre " + p.Nombre + ", Email " + p.Email + ", Edad " + p.Edad
+                + ", Aficiones " + aficiones;
+            return View();
+        }
+
+        //VAMOS A RECIBIR DOS PARAMETROS
+        public IActionResult VistaControllerGet(string saludo, System.Nullable<int> year)
+        {
+            if(year != null)
+            {
+                ViewData["DATA"] = "Hola " + saludo + " en el año " + year;
+            }
+            else
+            {
+                ViewData["DATA"] = "Aqui no tenemos tiempo ya..";
+            }
+            
             return View();
         }
         
