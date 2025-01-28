90461ce1686a49473a8c75ee0ae9f966776c31a6
CPenalba <carolinapenalbacorpa@gmail.com>
2025-01-28 13:25:06 +0100

    Sumar Numeros

---
+++ PrimerMvcNetCore/Controllers/MatematicasController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class MatematicasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SumarNumerosGet(int n1,int n2)
+        {
+            int suma = n1 + n2;
+            ViewData["MENSAJE"] = "La suma de " + n1 + " + " + n2 + " = " + suma;
+            return View();
+        }
+
+        public IActionResult SumarNumerosPost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SumarNumerosPost(int n1, int n2)
+        {
+            int suma = n1 + n2;
+            ViewData["MENSAJE"] = "La suma de " + n1 + " + " + n2 + " = " + suma;
+            return View();
+        }
+    }
+}
